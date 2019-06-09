57c1124c03f1509cef9d9f62c7c13e152cf1846c
yuhei1018 <shimmyo.yuhei1018@gmail.com>
2019-06-09 14:10:38 +0000

    test login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,18 @@
 @section('content')
     <h1>お店一覧</h1>
 
+    @guest
+    <div class="pb-3 w-50 h-100 my-0 mx-auto">
+      <form method='POST' action="{{ route('login') }}">
+         @csrf
+         <input id="email" type="hidden" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="test@test" required autofocus>
+         <input id="password" type="hidden" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="testtest" required>
+         <button class="btn btn-success">
+           <i class="fas fa-check"></i>{{ __(' test user login (ポートフォリオ閲覧用)') }}
+         </button>
+       </form>
+      </div>
+      @endguest
   <table class="table table-striped table-hover">
       <tr>
         <th>カテゴリ</th><th>店名</th><th>住所</th><th>投稿者</th>
